3b7213f4643938ddd06f2a89366ef24acd9436a1
unstoppableworm <unstoppableworm@gmail.com>
2025-07-06 20:51:13 +0900

    fix: minio 접근 url 임시 수정

--- src/main/java/com/sparrows/board/minio/config/MinioConfig.java
+++ src/main/java/com/sparrows/board/minio/config/MinioConfig.java
@@ -7,6 +7,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 
 import java.net.URI;
 
@@ -22,7 +23,10 @@ public class MinioConfig {
                 .credentialsProvider(StaticCredentialsProvider.create(
                         AwsBasicCredentials.create(properties.getAccessKey(), properties.getSecretKey())))
                 .endpointOverride(URI.create(properties.getEndpoint())) // MinIO는 custom endpoint
-                .region(Region.US_EAST_1) // 아무거나 가능, MinIO는 무시함
+                .region(Region.US_EAST_1) // 아무거나 가능, MinIO는 무시함\
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(true) // 핵심 설정
+                        .build())
                 .build();
     }
 }
